84a729592ce630f2c023e5a41d29284faf154529
Megan McD <mcdevittbass@gmail.com>
2020-08-03 14:04:26 -0400

    troubleshoot

--- mybot/features/dictionary.js
+++ mybot/features/dictionary.js
@@ -1,7 +1,9 @@
+//const thesaurus = require("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=e569694b-2440-4b3a-a5e2-b5d8fd6f1ca7")
+
 module.exports = function(controller) {
 
     controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
         await bot.reply(message, 'I heard "foo" via a function test');
     });
-    
+
 }
