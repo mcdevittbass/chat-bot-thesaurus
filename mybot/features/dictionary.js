1a882f8db556b8155a9d95b4031c64892df8f64c
Megan McD <mcdevittbass@gmail.com>
2020-08-03 13:36:24 -0400

    Heroku setup

---
+++ mybot/features/dictionary.js
@@ -0,0 +1,7 @@
+module.exports = function(controller) {
+
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
+        await bot.reply(message, 'I heard "foo" via a function test');
+    });
+    
+}
