cf6bd7bc89520e9b301925439fc9dc08cbe44e32
Megan McD <mcdevittbass@gmail.com>
2020-08-06 11:49:11 -0400

    Functional Thesaurus

--- mybot/features/thesaurus.js
+++ mybot/features/thesaurus.js
@@ -1,28 +1,46 @@
-const https = require("https");
-const thesaurus = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=e569694b-2440-4b3a-a5e2-b5d8fd6f1ca7";
+const fetch = require('node-fetch');
+const key = require("../api-key");
+const url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"
 
-
-let syns = '';
-https.get(thesaurus, async resp => {
-    let data = '';
-
-    await resp.on('data', (chunk) => {
-        data += chunk; 
-    });
-
-    await resp.on('end', () => {
-        syns = JSON.parse(data).syns;
-        console.log(syns);
-    });
-
-}).on('err', err => {
-    console.log("Error: " + err.message);
-  });
+//endpoint = url + word + ? + key
 
 module.exports = function(controller) {
+    const wordArr = [];
+    let x = 1;
 
-    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'syn', ['message'], async (bot, message) => {
-        await bot.reply(message, syns);
+    const callThesaurus = async (uri, i, word) => {
+        const response = await fetch(uri).catch(err => new Error("Problem getting results: " + err));
+        const data = await response.json().catch(err => new Error("Problem parsing results: " + err));
+        if(data[i]) {
+            if(!data[i].meta) {
+            return `Can't find that word. Did you mean: ${data.join(', ')}?`
+            } else {
+                return `Here are some synonymns for ${word}: ${data[i].meta.syns[0].join(', ')}`;
+            }
+        } else {
+            return 'NO MORE!'
+        }
+    }
+    controller.on('message,direct_message', async (bot, message) => {
+        const word = await message.text;
+        wordArr.push(word);
+        const endpoint = url + word + '?' + key;
+        const synonyms = await callThesaurus(endpoint, 0, word).catch(err => new Error("Couldn't find that word! Try another one."));
+        await bot.reply(message, synonyms);
+        if(synonyms.includes('Here are some')) {
+            await bot.reply(message, "If you want more synonmns, type 'more please'");
+        }
     });
 
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'more please', ['message'], async(bot, message) => {
+        let word = wordArr[wordArr.length - 1];
+        console.log(word);
+        const endpoint = url + word + '?' + key;
+        const synonyms = await callThesaurus(endpoint, x, word).catch(err => new Error("Couldn't find that word! Try another one."));
+        await bot.reply(message, synonyms);
+        if(synonyms !== 'NO MORE!') {
+            await bot.reply(message, "If you want more synonmns, type 'more please'");
+        }
+        x += 1;
+    }) 
 }
